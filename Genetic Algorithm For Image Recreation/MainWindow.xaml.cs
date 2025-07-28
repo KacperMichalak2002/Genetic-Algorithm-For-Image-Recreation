11e7a7e84fee47d113ffa4ffd13043a9a815daeb
Kacper Michalak <michalakkacper2002@gmail.com>
2025-07-28 19:27:45 +0200

    Add project files.

---
+++ Genetic Algorithm For Image Recreation/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Genetic_Algorithm_For_Image_Recreation
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
